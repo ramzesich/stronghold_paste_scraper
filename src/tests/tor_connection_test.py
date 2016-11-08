dd9c66e46024ff8a2b78bab01a0b56264b731cce
shemsu <ramzesich@gmail.com>
2016-11-08 10:03:35 +0200

    test code organization

--- src/tests/tor_connection_test.py
+++ src/tests/tor_connection_test.py
@@ -1,6 +1,13 @@
 import requests
 
+
 proxies = {'http': 'socks5://localhost:9050',
            'https': 'socks5://localhost:9050'}
 
-assert requests.get('http://nzxj65x32vh2fkhk.onion/all', proxies=proxies, timeout=24).status_code == 200
+
+def run():
+    assert requests.get('http://nzxj65x32vh2fkhk.onion/all', proxies=proxies, timeout=24).status_code == 200
+
+
+if __name__ == '__main__':
+    run()
